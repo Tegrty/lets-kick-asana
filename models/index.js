4fb96f7a985fd0fde4829e10db927bd8643ede4e
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-01 07:42:06 -0800

    index and pose models complete

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
+const Pose = require('./Pose');
 
-module.exports = { User };
+User.hasMany(Pose, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Pose.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Pose };
