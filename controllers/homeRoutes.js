6eef77bc113a0ea092e1fa227989ac5496f43227
Mario Repas <mario.repas63@gmail.com>
2023-03-02 18:43:23 -0500

    added basic dashboard layout, confirmed working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ const withAuth = require('../utils/auth');
 
 // Get route for all poses with auth
 
-router.get('/gallery', async (req, res) => {
+router.get('/gallery',withAuth, async (req, res) => {
     try {
         const poseData = await Pose.findAll({
                 
@@ -58,6 +58,28 @@ router.get('/', (req, res) => {
     }
 
     res.render('homepage');
+});
+
+// Get dashboard with auth
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+
+
+            attributes: { exclude: ['password'] },
+            include: [{ model: Pose }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
